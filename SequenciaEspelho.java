9b8f7570b1117614634545cd7d82043ddea95cd0
Samuel Lucas <samuel.rod.vieira@gmail.com>
2025-09-06 18:45:14 -0300

    Sequência Espelho

---
+++ SequenciaEspelho.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+
+public class SequenciaEspelho {
+
+    static int invert(int n) {
+        int inv = 0;
+        while (n > 0) {
+            inv = inv * 10 + n % 10;
+            n /= 10;
+        }
+        return inv;
+    }
+
+    static void mirror(int[] vet, int s, int e, int tam) {
+        int cont1 = s, cont2 = e;
+        for (int i = 0; i < tam; i++) {
+            if (i < tam / 2) {
+                vet[i] = cont1;
+                cont1++;
+            } else {
+                int original = cont2;
+                int inverted = invert(original);
+                if (original % 10 == 0) {
+                    vet[i] = -inverted;
+                } else {
+                    vet[i] = inverted;
+                }
+                cont2--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int s = ler.nextInt();
+        int e = ler.nextInt();
+
+        int tam = 2 * (e - s + 1);
+
+        int[] vet = new int[tam];
+        mirror(vet, s, e, tam);
+
+        for (int i = 0; i < tam; i++) {
+            if (vet[i] < 0) {
+                System.out.print("0" + (-vet[i]));
+            } else {
+                System.out.print(vet[i]);
+            }
+        }
+
+        ler.close();
+    }
+}
